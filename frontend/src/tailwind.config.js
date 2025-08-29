4b21d47cd010c4571ebef3c517564ef7ade450b0
Carey Gatz <carey@teamstransport.com>
2025-08-29 12:37:21 -0500

    updated to use vite

--- frontend/src/tailwind.config.js
+++ frontend/src/tailwind.config.js
@@ -1,12 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    // CRA:
-    './public/index.html',
+    './index.html',
     './src/**/*.{js,jsx,ts,tsx}',
-
-    // If using Vite (instead of CRA), also include the root index.html:
-    // './index.html',
   ],
   theme: {
     extend: {},
