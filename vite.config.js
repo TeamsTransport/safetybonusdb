9b2ff68cf7c577c6fbe0e1d6e2a633a4d78664f4
Carey Gatz <carey@teamstransport.com>
2025-09-08 11:03:23 -0500

    fixed syntax errors indexcss tailwindcss

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
+import tailwind from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(),
+    tailwind(),
   ],
   server: {
     host: '0.0.0.0',
