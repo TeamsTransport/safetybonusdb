1544921266668c5550d2eaf4423b19cf932e5116
Carey Gatz <carey@teamstransport.com>
2025-09-08 15:45:58 -0500

    adding DaisyUI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import DriverList from './components/DriverList';
 import TruckList from './components/TruckList';
 import SafetyEventList from './components/SafetyEventList';
+import TestDaisy from './components/TestDaisy'
 
 export default function App() {
   return (
     <div className="p-5 max-w-screen-xl mx-auto">
       <h1>Safety Bonus Dashboard</h1>
+      <TestDaisy />
+      <hr/>
       <DriverList />
       <hr/>
       <TruckList />
